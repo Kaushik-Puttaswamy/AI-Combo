2fa857998034085c844e4ac5bb4fe89afb8d9c4a
Kaushik Puttaswamy <123877618+Kaushik-Puttaswamy@users.noreply.github.com>
2026-03-09 23:12:42 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+hu/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
@@ -15,6 +15,5 @@ export default {
         }
       }
     },
-  },
-  plugins: [],
+
 }
